40c4be5c316f8ff42abc0160cea3980a70d7f2b3
SolbiShin <sbshin.dev@gmail.com>
2021-12-06 23:26:40 +0900

    정적 파일 제공하기

--- src/index.server.js
+++ src/index.server.js
@@ -3,6 +3,44 @@ import ReactDOMServer from 'react-dom/server';
 import express from 'express';
 import {StaticRouter} from 'react-router-dom';
 import App from './App';
+import path from 'path';
+import fs from 'fs';
+
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$/.exec(key))
+  .map(key => `<script src="${manifest.files[key]}"></script>`)
+  .join('');
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8 />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1, shrink-to-fit=no"
+      />
+      <meta name="theme-color" content="#000000" />
+      <title>React App</title>
+      <link href="${manifest.files['main.css']}" rel="stylesheet" />
+    </head>
+    <body>
+      <noscript>You need to enable Javascript to run this app.</noscript>
+      <div id="root">
+        ${root}
+      </div>
+      <script src="${manifest.files['runtime-main.js']}"></script>
+      ${chunks}
+      <script src="${manifest.files['main.js']}"></script>
+     </body>
+     </html>
+     `;
+}
 
 const app = express();
 
@@ -16,9 +54,14 @@ const serverRender = (req, res, next) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx); // 렌더링하고
-  res.send(root); // 클라이언트에 결과 응답
+  res.send(createPage(root)); // 클라이언트에 결과 응답
 };
 
+const serve = express.static(path.resolve('./build'), {
+  index: false 
+});
+
+app.use(serve);
 app.use(serverRender);
 
 // 5000 포트로 서버 가동
