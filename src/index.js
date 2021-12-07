98e5c30a591926ec8ffba4e8396424ff2ff7e0f8
SolbiShin <sbshin.dev@gmail.com>
2021-12-08 02:04:13 +0900

    ssr &  코드 스플리팅

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from '@redux-saga/core';
 import rootReducer, {rootSaga} from './modules';
+import { loadableReady } from '@loadable/component';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -19,15 +20,27 @@ const store = createStore(
  );
 
  sagaMiddleware.run(rootSaga);
+
  
-ReactDOM.render(
-  <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+ const Root = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+ };
+
+ const root = document.getElementById('root');
+
+ if(process.env.NODE_ENV === 'production') {
+   loadableReady(() => {
+     ReactDOM.hydrate(<Root />, root);
+   });
+ } else {
+   ReactDOM.render(<Root />, root);
+ }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
