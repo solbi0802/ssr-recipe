2c14687fea7e0bc1d72bfc1ef456e53c616f9db8
SolbiShin <sbshin.dev@gmail.com>
2021-12-07 23:41:03 +0900

    redux-thunk로 user page 만들기

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import { createStore, applyMiddleware} from 'redux';
+import { Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/pages/UsersPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import UsersContainer from '../containers/UsersContainer';
+
+const USersPage = () => {
+    return <UsersContainer />;
+};
+
+export default USersPage;
